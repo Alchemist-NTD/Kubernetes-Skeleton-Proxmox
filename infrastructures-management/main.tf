5bf00f6788ed4f1bcf5ce9f8b00c94e49b32e3d1
duy <nguyenthanhduy030898@gmail.com>
2024-02-21 20:55:08 +0700

    refactor prometheus ansible role

--- infrastructures-management/main.tf
+++ infrastructures-management/main.tf
@@ -1,58 +1,58 @@
-module "kubernetes-master-nodes" {
-    source = "./module/kubernetes"
+# module "kubernetes-master-nodes" {
+#     source = "./module/kubernetes"
 
-    node_count    = 3
-    node_type     = "master"
-    node_desc     = "k8s ubuntu server master nodes"
-    node_memory   = 4096
-    node_ip_base  = "192.168.1.11"
-    node_disk_size = "32G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+#     node_count    = 3
+#     node_type     = "master"
+#     node_desc     = "k8s ubuntu server master nodes"
+#     node_memory   = 4096
+#     node_ip_base  = "192.168.1.11"
+#     node_disk_size = "32G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
 
-module "kubernetes-worker-nodes" {
-    source = "./module/kubernetes"
+# module "kubernetes-worker-nodes" {
+#     source = "./module/kubernetes"
 
-    node_count    = 3
-    node_type     = "worker"
-    node_desc     = "k8s ubuntu server worker nodes"
-    node_memory   = 4096
-    node_ip_base  = "192.168.1.12"
-    node_disk_size = "32G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+#     node_count    = 3
+#     node_type     = "worker"
+#     node_desc     = "k8s ubuntu server worker nodes"
+#     node_memory   = 4096
+#     node_ip_base  = "192.168.1.12"
+#     node_disk_size = "32G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
 
-module "kubernetes-load-balancer" {
-    source = "./module/kubernetes"
+# module "kubernetes-load-balancer" {
+#     source = "./module/kubernetes"
 
-    node_count    = 1
-    node_type     = "load-balancer"
-    node_desc     = "k8s ubuntu server load balancer"
-    node_memory   = 2048
-    node_ip_base  = "192.168.1.13"
-    node_disk_size = "16G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+#     node_count    = 1
+#     node_type     = "load-balancer"
+#     node_desc     = "k8s ubuntu server load balancer"
+#     node_memory   = 2048
+#     node_ip_base  = "192.168.1.13"
+#     node_disk_size = "16G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
 
-module "kubespray-server" {
-    source = "./module/kubernetes"
+# module "kubespray-server" {
+#     source = "./module/kubernetes"
 
-    node_count    = 1
-    node_type     = "kubespray-server"
-    node_desc     = "kubespray server"
-    node_memory   = 2048
-    node_ip_base  = "192.168.1.10"
-    node_disk_size = "16G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+#     node_count    = 1
+#     node_type     = "kubespray-server"
+#     node_desc     = "kubespray server"
+#     node_memory   = 2048
+#     node_ip_base  = "192.168.1.10"
+#     node_disk_size = "16G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
 
 module "single-nodes" {
     source = "./module/kubernetes"

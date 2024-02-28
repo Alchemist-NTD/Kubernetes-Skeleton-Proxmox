d8375d7cee5f4cfc392fcd51c423fe2946a65506
duy <nguyenthanhduy030898@gmail.com>
2024-02-28 23:43:10 +0700

    refactor some kubernetes task into new roles

--- configurations-management/install.sh
+++ configurations-management/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-tar -cvf ../kubespray.tar ../kubespray
+# tar -cvf ../kubespray.tar ../kubespray
 
-ansible-playbook -i ./inventory/hosts playbook.yaml
+rm -rf ~/.ssh/known_hosts
 
-ansible-playbook -i ./inventory/hosts node-exporter.yaml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory/lite-hosts.yaml playbook.yaml
 
-ansible-playbook -i ./inventory/hosts media-server-playbook.yaml
+# ansible-playbook -i ./inventory/hosts node-exporter.yaml
+
+# ansible-playbook -i ./inventory/hosts media-server-playbook.yaml
--- infrastructures-management/main.tf
+++ infrastructures-management/main.tf
@@ -1,44 +1,46 @@
-# module "kubernetes-master-nodes" {
-#     source = "./module/kubernetes"
+module "kubernetes-master-nodes" {
+    source = "./module/kubernetes"
 
-#     node_count    = 3
-#     node_type     = "master"
-#     node_desc     = "k8s ubuntu server master nodes"
-#     node_memory   = 4096
-#     node_ip_base  = "192.168.1.11"
-#     node_disk_size = "32G"
-#     template_name = var.template_name
-#     vm_password   = var.vm_password
-#     ssh_key       = var.ssh_key
-# }
+    # node_count    = 3
+    node_count    = 1
+    node_type     = "master"
+    node_desc     = "k8s ubuntu server master nodes"
+    node_memory   = 4096
+    node_ip_base  = "192.168.1.11"
+    node_disk_size = "32G"
+    template_name = var.template_name
+    vm_password   = var.vm_password
+    ssh_key       = var.ssh_key
+}
 
-# module "kubernetes-worker-nodes" {
-#     source = "./module/kubernetes"
+module "kubernetes-worker-nodes" {
+    source = "./module/kubernetes"
 
-#     node_count    = 3
-#     node_type     = "worker"
-#     node_desc     = "k8s ubuntu server worker nodes"
-#     node_memory   = 4096
-#     node_ip_base  = "192.168.1.12"
-#     node_disk_size = "32G"
-#     template_name = var.template_name
-#     vm_password   = var.vm_password
-#     ssh_key       = var.ssh_key
-# }
+    # node_count    = 3
+    node_count    = 2
+    node_type     = "worker"
+    node_desc     = "k8s ubuntu server worker nodes"
+    node_memory   = 4096
+    node_ip_base  = "192.168.1.12"
+    node_disk_size = "32G"
+    template_name = var.template_name
+    vm_password   = var.vm_password
+    ssh_key       = var.ssh_key
+}
 
-# module "kubernetes-load-balancer" {
-#     source = "./module/kubernetes"
+module "kubernetes-load-balancer" {
+    source = "./module/kubernetes"
 
-#     node_count    = 1
-#     node_type     = "load-balancer"
-#     node_desc     = "k8s ubuntu server load balancer"
-#     node_memory   = 2048
-#     node_ip_base  = "192.168.1.13"
-#     node_disk_size = "16G"
-#     template_name = var.template_name
-#     vm_password   = var.vm_password
-#     ssh_key       = var.ssh_key
-# }
+    node_count    = 1
+    node_type     = "load-balancer"
+    node_desc     = "k8s ubuntu server load balancer"
+    node_memory   = 2048
+    node_ip_base  = "192.168.1.13"
+    node_disk_size = "16G"
+    template_name = var.template_name
+    vm_password   = var.vm_password
+    ssh_key       = var.ssh_key
+}
 
 # module "kubespray-server" {
 #     source = "./module/kubernetes"
@@ -54,30 +56,30 @@
 #     ssh_key       = var.ssh_key
 # }
 
-module "jellyfin-nodes" {
-    source = "./module/kubernetes"
+# module "jellyfin-nodes" {
+#     source = "./module/kubernetes"
 
-    node_count    = 1
-    node_type     = "jellyfin"
-    node_desc     = "jellyfin host nodes"
-    node_memory   = 8192
-    node_ip_base  = "192.168.1.9"
-    node_disk_size = "32G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+#     node_count    = 1
+#     node_type     = "jellyfin"
+#     node_desc     = "jellyfin host nodes"
+#     node_memory   = 8192
+#     node_ip_base  = "192.168.1.9"
+#     node_disk_size = "32G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
 
-module "minio-nodes" {
-    source = "./module/kubernetes"
+# module "minio-nodes" {
+#     source = "./module/kubernetes"
 
-    node_count    = 1
-    node_type     = "minio"
-    node_desc     = "minio storage nodes"
-    node_memory   = 8192
-    node_ip_base  = "192.168.1.8"
-    node_disk_size = "128G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+#     node_count    = 1
+#     node_type     = "minio"
+#     node_desc     = "minio storage nodes"
+#     node_memory   = 8192
+#     node_ip_base  = "192.168.1.8"
+#     node_disk_size = "128G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
